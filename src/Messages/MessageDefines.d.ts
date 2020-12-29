3245deddb4ef3615ba2a35a3ca73269ee6f16d59
Lam Le Viet <zellitsss@gmail.com>
2020-12-29 16:30:01 +0700

    Update messages template

--- src/Messages/MessageDefines.d.ts
+++ src/Messages/MessageDefines.d.ts
@@ -175,6 +175,181 @@ export class Client_LeaveRoom implements IClient_LeaveRoom {
     public toJSON(): { [k: string]: any };
 }
 
+/** Represents a Vec2. */
+export class Vec2 implements IVec2 {
+
+    /**
+     * Constructs a new Vec2.
+     * @param [properties] Properties to set
+     */
+    constructor(properties?: IVec2);
+
+    /** Vec2 x. */
+    public x: number;
+
+    /** Vec2 y. */
+    public y: number;
+
+    /**
+     * Creates a new Vec2 instance using the specified properties.
+     * @param [properties] Properties to set
+     * @returns Vec2 instance
+     */
+    public static create(properties?: IVec2): Vec2;
+
+    /**
+     * Encodes the specified Vec2 message. Does not implicitly {@link Vec2.verify|verify} messages.
+     * @param message Vec2 message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encode(message: IVec2, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Encodes the specified Vec2 message, length delimited. Does not implicitly {@link Vec2.verify|verify} messages.
+     * @param message Vec2 message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encodeDelimited(message: IVec2, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Decodes a Vec2 message from the specified reader or buffer.
+     * @param reader Reader or buffer to decode from
+     * @param [length] Message length if known beforehand
+     * @returns Vec2
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): Vec2;
+
+    /**
+     * Decodes a Vec2 message from the specified reader or buffer, length delimited.
+     * @param reader Reader or buffer to decode from
+     * @returns Vec2
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): Vec2;
+
+    /**
+     * Verifies a Vec2 message.
+     * @param message Plain object to verify
+     * @returns `null` if valid, otherwise the reason why it is not
+     */
+    public static verify(message: { [k: string]: any }): (string|null);
+
+    /**
+     * Creates a Vec2 message from a plain object. Also converts values to their respective internal types.
+     * @param object Plain object
+     * @returns Vec2
+     */
+    public static fromObject(object: { [k: string]: any }): Vec2;
+
+    /**
+     * Creates a plain object from a Vec2 message. Also converts values to other types if specified.
+     * @param message Vec2
+     * @param [options] Conversion options
+     * @returns Plain object
+     */
+    public static toObject(message: Vec2, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+    /**
+     * Converts this Vec2 to JSON.
+     * @returns JSON object
+     */
+    public toJSON(): { [k: string]: any };
+}
+
+/** Represents a PlayerState. */
+export class PlayerState implements IPlayerState {
+
+    /**
+     * Constructs a new PlayerState.
+     * @param [properties] Properties to set
+     */
+    constructor(properties?: IPlayerState);
+
+    /** PlayerState id. */
+    public id: string;
+
+    /** PlayerState name. */
+    public name: string;
+
+    /** PlayerState position. */
+    public position?: (IVec2|null);
+
+    /**
+     * Creates a new PlayerState instance using the specified properties.
+     * @param [properties] Properties to set
+     * @returns PlayerState instance
+     */
+    public static create(properties?: IPlayerState): PlayerState;
+
+    /**
+     * Encodes the specified PlayerState message. Does not implicitly {@link PlayerState.verify|verify} messages.
+     * @param message PlayerState message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encode(message: IPlayerState, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Encodes the specified PlayerState message, length delimited. Does not implicitly {@link PlayerState.verify|verify} messages.
+     * @param message PlayerState message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encodeDelimited(message: IPlayerState, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Decodes a PlayerState message from the specified reader or buffer.
+     * @param reader Reader or buffer to decode from
+     * @param [length] Message length if known beforehand
+     * @returns PlayerState
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): PlayerState;
+
+    /**
+     * Decodes a PlayerState message from the specified reader or buffer, length delimited.
+     * @param reader Reader or buffer to decode from
+     * @returns PlayerState
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): PlayerState;
+
+    /**
+     * Verifies a PlayerState message.
+     * @param message Plain object to verify
+     * @returns `null` if valid, otherwise the reason why it is not
+     */
+    public static verify(message: { [k: string]: any }): (string|null);
+
+    /**
+     * Creates a PlayerState message from a plain object. Also converts values to their respective internal types.
+     * @param object Plain object
+     * @returns PlayerState
+     */
+    public static fromObject(object: { [k: string]: any }): PlayerState;
+
+    /**
+     * Creates a plain object from a PlayerState message. Also converts values to other types if specified.
+     * @param message PlayerState
+     * @param [options] Conversion options
+     * @returns Plain object
+     */
+    public static toObject(message: PlayerState, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+    /**
+     * Converts this PlayerState to JSON.
+     * @returns JSON object
+     */
+    public toJSON(): { [k: string]: any };
+}
+
 /** Represents a GameState. */
 export class GameState implements IGameState {
 
@@ -186,6 +361,9 @@ export class GameState implements IGameState {
 
     /** GameState mesageType. */
     public mesageType: MessageType;
+
+    /** GameState players. */
+    public players: IPlayerState[];
 
     /**
      * Creates a new GameState instance using the specified properties.
